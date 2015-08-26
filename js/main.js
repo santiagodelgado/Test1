85df3be6bd9b9dee2e9b6d3999e555317576337e
santiagodelgado <contacto@santiagodelgado.es>
2015-08-26 18:53:10 +0200

    web real

--- js/main.js
+++ js/main.js
@@ -16,7 +16,8 @@ var app = {
 			var request = $.ajax({
 				type: "POST" ,
 				crossDomain: true,
-				url: "http://192.168.1.10/app_dev.php/mobileapp/" + usuario + "/" + pass
+				//url: "http://192.168.1.10/app_dev.php/mobileapp/" + usuario + "/" + pass
+				url: "http://santiagodelgado.es/PFG/web/mobileapp/" + usuario + "/" + pass
 			});
 
 			request.done(function (response, textStatus, jqXHR){
